4dda505bac2b25645b4ceba6a3289668fca7bf7f
Abdullah Nassar <abdullah.u.nassar@vanderbilt.edu>
2024-10-31 22:37:42 +0300

    Stripe integration

--- web/accounts/models.py
+++ web/accounts/models.py
@@ -1,3 +1,5 @@
+import stripe
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.crypto import get_random_string
@@ -16,15 +18,15 @@ class Team(models.Model):
 
 class CustomUser(AbstractUser):
     TIER = (
+        ("free", "Free"),
         ("individual", "Individual"),
         ("team", "Team"),
     )
     subscribe_to_emails = models.BooleanField(default=True)
-    tier = models.CharField(max_length=50, choices=TIER, default="individual")
+    tier = models.CharField(max_length=50, choices=TIER, default="free")
     stripe_customer_id = models.CharField(max_length=255, blank=True)
     stripe_subscription_id = models.CharField(max_length=255, blank=True)
     token = models.CharField(max_length=255, blank=True)
-    consumed_credits = models.IntegerField(default=0)
     available_credits = models.IntegerField(default=1000)  # free 1000 credits on signup
     team = models.ForeignKey(
         Team, on_delete=models.SET_NULL, null=True, blank=True, related_name="members"
@@ -50,11 +52,6 @@ class CustomUser(AbstractUser):
             return self.team.owner.available_credits
         return self.available_credits
 
-    def get_consumed_credits(self):
-        if self.team:
-            return self.team.owner.consumed_credits
-        return self.consumed_credits
-
     def set_available_credits(self, credits):
         if self.team:
             self.team.owner.available_credits = credits
@@ -63,10 +60,15 @@ class CustomUser(AbstractUser):
             self.available_credits = credits
             self.save()
 
-    def set_consumed_credits(self, credits):
-        if self.team:
-            self.team.owner.consumed_credits = credits
-            self.team.owner.save()
-        else:
-            self.consumed_credits = credits
-            self.save()
+    def record_consumed_credits(self, credits):
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        customer_id = self.stripe_customer_id
+        # customer_id = "cus_R8JkMI5v4oBQiB"  # for testing
+
+        if self.tier == "team":
+            customer_id = self.team.owner.stripe_customer_id
+
+        stripe.billing.MeterEvent.create(
+            event_name=settings.STRIPE_METER_EVENT,
+            payload={"value": credits, "stripe_customer_id": customer_id},
+        )
