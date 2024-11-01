d086e6346e050aa0c56faf46bbe1b0b2e6a66114
Abdullah Nassar <abdullah.u.nassar@vanderbilt.edu>
2024-11-01 13:49:31 +0200

    Add stripe portal

--- web/frontend/urls.py
+++ web/frontend/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("stripe-checkout/", views.stripe_checkout, name="stripe_checkout"),
-    path("customer-portal/", views.customer_portal, name="customer_portal"),
+    path("stripe-portal/", views.customer_portal, name="customer_portal"),
     path("stripe-webhook/", views.stripe_webhook, name="stripe_webhook"),
     path("payment-success/", views.payment_success, name="payment_success"),
     path("payment-cancel/", views.payment_cancel, name="payment_cancel"),
